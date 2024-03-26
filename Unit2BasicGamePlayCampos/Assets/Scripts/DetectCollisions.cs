f4ff7d1d347fc4aac54ae5a6c43ad731edd0c646
Gerardo Campos <751033@student.dusd.net>
2024-03-25 22:42:31 -0700

    FInished Unit 2

---
+++ Unit2BasicGamePlayCampos/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    private static int lives = 3;
+    private static int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+        lives--;
+        Debug.Log("Lives:" + lives);
+            if (lives == 0)
+            {
+                Destroy(other.gameObject);
+                Debug.Log("Game Over!");
+            }
+        }
+
+        else if (other.gameObject.name != "Player")
+        {
+            score++;
+            Destroy(other.gameObject);
+            Debug.Log("Score:" + score);
+        }
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
--- Unit2BasicGamePlayCampos/Assets/Scripts/PlayerController.cs
+++ Unit2BasicGamePlayCampos/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
 
@@ -19,6 +20,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 playerMovement = new Vector3(horizontalInput, 0, verticalInput);
+
+        transform.Translate(Vector3.ClampMagnitude(playerMovement, 1) * (speed * Time.deltaTime));
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -31,7 +38,7 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.A))
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
